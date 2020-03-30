db2f97c3e2dc21912dd06df0139e4da38fa7e2c8
Sam-Glerum <samuelglerum@hotmail.com>
2020-03-30 12:36:06 +0200

    created basic server setup

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ let express = require('express');
 
 let index = express();
 
-index.get("/api", (req, res) => {
-    res.status(200).json({"Response" : "API works!"})
+index.get("*", (req, res) => {
+    res.status(200).json({"response" : "API works!"})
+});
+
+index.listen(3000, () => {
+    console.log("Server is listening on port 3000")
 });
