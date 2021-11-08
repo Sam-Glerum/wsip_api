a1c02a8e8810b9c0f6f8df62c6fb93acce6c79d3
Sam Glerum <samuelglerum@hotmail.com>
2021-11-08 23:09:04 +0100

    updated index.js. split up 'app' and 'server' into separate objects for testing purposes

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
+const http = require('http');
 const bodyParser = require('body-parser');
-const index = express();
+const app = express();
 const cors = require('cors');
 
-// Enable bodyParser, so that we can parse json from incoming requests
-index.use(bodyParser.json());
-// Enable CORS
-index.use(cors());
-// Load routes
-index.use("/api", require("./routes/v1/steam_request_routes"));
+const server = http.createServer(app);
 
-index.get("*", (req, res) => {
+// Enable bodyParser, so that we can parse json from incoming requests
+app.use(bodyParser.json());
+// Enable CORS
+app.use(cors());
+// Load routes
+app.use("/api", require("./routes/v1/steam_request_routes"));
+
+app.get("*", (req, res) => {
     res.status(200).json({"response" : "Welcome to the WSIP api!"})
 });
 
 // Set the port
-index.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening on port " + (process.env.PORT || 3000));
 });
 
-module.exports = index;
+module.exports = app;
